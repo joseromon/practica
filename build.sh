7c463a077a2b3f767ccd8188fa002afd703fc065
joseromon <jmrg12@educa.madrid.org>
2024-11-16 15:12:58 +0100

    descarga de w3.css

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ mkdir -p build
 cp -r index.html page1.html page2.html build/
 echo 
 
+echo "Descargando w3.css desde $W3CSS_URL..."
+if ! curl -o $W3CSS_FILE $W3CSS_URL; then
+    echo "Error: No se pudo descargar w3.css."
+    exit 1
+fi
